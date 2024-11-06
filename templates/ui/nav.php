6017ef23d4f1b9c817e928829c07634e4488d654
Evans PARFAIT <fani.prft@gmail.com>
2024-11-06 23:33:02 +0100

    third commit

--- templates/ui/nav.php
+++ templates/ui/nav.php
@@ -1,5 +1,8 @@
 <nav class="navbar">
     <a href="/account" style="color: #fff; text-decoration: none;">Informations du compte</a>
-    <h1>Blog</h1>
+    <div class="title-group">
+        <h1>Blog</h1>
+        <p>Bienvenue <span><?= $_SESSION['firstname'] ?></span></p>
+    </div>
     <button type="button" class="btn btn-light"><a href="/logout">Logout</a></button>
 </nav>
